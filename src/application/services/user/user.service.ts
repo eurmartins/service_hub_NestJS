a4c11eea593c4bbb0815209469f67c6a83769322
Eurmartins <victormartinssantos.work@gmail.com>
2025-10-26 17:11:23 -0300

    refactor: enhance error handling and logging in UserService methods

--- src/application/services/user/user.service.ts
+++ src/application/services/user/user.service.ts
@@ -19,46 +19,89 @@ export class UserService {
   }
 
   async create(createUserDto: CreateUserDto): Promise<User> {
-    this.logger.info(`Creating user with email: ${createUserDto.email}`);
-    const user = new User();
-    Object.assign(user, createUserDto);
-    user.hashSenha = await this.hashService.hash(createUserDto.hashSenha);
-    const savedUser = await this.userRepository.save(user);
-    this.logger.info(`User created with ID: ${savedUser.id}`);
-    return savedUser;
+    try {
+      this.logger.info(`Creating user with email: ${createUserDto.email}`);
+
+      const user = new User();
+      Object.assign(user, createUserDto);
+      user.hashSenha = await this.hashService.hash(createUserDto.hashSenha);
+      const savedUser = await this.userRepository.save(user);
+
+      this.logger.info(`User created with ID: ${savedUser.id}`);
+
+      return savedUser;
+    } catch (error) {
+      this.logger.error(
+        `Error creating user with email: ${createUserDto.email}`,
+      );
+      throw error;
+    }
   }
 
   async findAll(): Promise<User[]> {
-    this.logger.info('Fetching all users');
-    const users = await this.userRepository.find();
-    this.logger.info(`Found ${users.length} users`);
-    return users;
+    try {
+      this.logger.info('Fetching all users');
+
+      const users = await this.userRepository.find();
+
+      this.logger.info(`Found ${users.length} users`);
+
+      return users;
+    } catch (error) {
+      this.logger.error(`Error to fetch all users. Error details: ${error}`);
+      throw error;
+    }
   }
 
   async findOne(id: string): Promise<User | null> {
-    this.logger.info(`Fetching user with ID: ${id}`);
-    const user = await this.userRepository.findOneBy({ id });
-    if (user) {
-      this.logger.info(`User found: ${user.email}`);
-    } else {
-      this.logger.error(`User with ID ${id} not found`);
+    try {
+      this.logger.info(`Fetching user with ID: ${id}`);
+
+      const user = await this.userRepository.findOneBy({ id });
+
+      if (user) {
+        this.logger.info(`User found: ${user.email}`);
+      } else {
+        this.logger.error(`User with ID ${id} not found`);
+      }
+
+      return user;
+    } catch (error) {
+      this.logger.error(`Error fetching user with ID ${id}. Error: ${error}`);
+      throw error;
     }
-    return user;
   }
 
   async update(id: string, updateUserDto: UpdateUserDto): Promise<User | null> {
-    this.logger.info(`Updating user with ID: ${id}`);
-    const existing = await this.findOne(id);
-    if (!existing) return null;
-    Object.assign(existing, updateUserDto);
-    const updatedUser = await this.userRepository.save(existing);
-    this.logger.info(`User updated: ${updatedUser.email}`);
-    return updatedUser;
+    try {
+      this.logger.info(`Updating user with ID: ${id}`);
+
+      const user_existing = await this.findOne(id);
+
+      if (!user_existing) return null;
+
+      Object.assign(user_existing, updateUserDto);
+      const updatedUser = await this.userRepository.save(user_existing);
+
+      this.logger.info(`User updated: ${updatedUser.email}`);
+
+      return updatedUser;
+    } catch (error) {
+      this.logger.error(`Error updating user with ID ${id}. Error: ${error}`);
+      throw error;
+    }
   }
 
   async remove(id: string): Promise<void> {
-    this.logger.info(`Deleting user with ID: ${id}`);
-    await this.userRepository.delete(id);
-    this.logger.info(`User deleted`);
+    try {
+      this.logger.info(`Deleting user with ID: ${id}`);
+
+      await this.userRepository.delete(id);
+
+      this.logger.info(`User deleted`);
+    } catch (error) {
+      this.logger.error(`Error deleting user with ID ${id}. Error: ${error}`);
+      throw error;
+    }
   }
 }

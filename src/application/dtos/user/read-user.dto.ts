bc2b205c27f37442f195b54388f2b2099faf1b33
Eurmartins <victormartinssantos.work@gmail.com>
2025-10-28 01:03:25 -0300

    feat: create ReadUserDto for user data transfer with validation

---
+++ src/application/dtos/user/read-user.dto.ts
@@ -0,0 +1,12 @@
+import { IsEmail, IsBoolean, IsUUID } from 'class-validator';
+
+export class ReadUserDto {
+  @IsUUID()
+  id: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsBoolean()
+  ativo: boolean;
+}

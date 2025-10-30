6e10541cd3506308f1da3ad2cd5e17bbad0ca37b
Eurmartins <victormartinssantos.work@gmail.com>
2025-10-30 02:58:59 -0300

    feat: enhance ReadClientDto with validation decorators for user and id

--- src/application/dtos/client/read-client.dto.ts
+++ src/application/dtos/client/read-client.dto.ts
@@ -1,7 +1,16 @@
+import { IsDefined, IsString, IsUUID, ValidateNested } from 'class-validator';
 import { ReadUserDto } from '../user/read-user.dto';
+import { Type } from 'class-transformer';
 
 export class ReadClientDto {
+  @IsUUID()
   id: string;
+
+  @IsDefined()
+  @ValidateNested()
+  @Type(() => ReadUserDto)
   user: ReadUserDto;
+
+  @IsString()
   name: string;
 }

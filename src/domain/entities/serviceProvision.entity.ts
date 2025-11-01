18b726e9d5ddcb78fcd4cef30a89dc6972d8b16e
Eurmartins <victormartinssantos.work@gmail.com>
2025-11-01 04:05:06 -0300

    feat: add ServiceProvision entity for managing service provision data

---
+++ src/domain/entities/serviceProvision.entity.ts
@@ -0,0 +1,65 @@
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { StatusEnum } from './enums/status.enum';
+import { Provider } from './provider.entity';
+import { Title } from '../values-objects/serviceProvision.values-objects/title.values-objects';
+import { Description } from '../values-objects/serviceProvision.values-objects/description.values-objects';
+import { Price } from '../values-objects/serviceProvision.values-objects/price.values-objects';
+
+@Entity()
+export class ServiceProvision {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  private _title: string;
+
+  @Column()
+  private _description: string;
+
+  @Column('decimal')
+  private _price: number;
+
+  @Column({
+    type: 'enum',
+    enum: StatusEnum,
+    default: StatusEnum.ACTIVE,
+  })
+  status: StatusEnum;
+
+  @Column('uuid')
+  providerId: string;
+
+  @ManyToOne(() => Provider)
+  @JoinColumn({ name: 'providerId' })
+  provider: Provider;
+
+  get title(): Title {
+    return new Title(this._title);
+  }
+
+  set title(title: Title) {
+    this._title = title.toString();
+  }
+
+  get description(): Description {
+    return new Description(this._description);
+  }
+
+  set description(description: Description) {
+    this._description = description.toString();
+  }
+
+  get price(): Price {
+    return new Price(this._price);
+  }
+
+  set price(price: Price) {
+    this._price = price.toNumber();
+  }
+}

afe401ff333b1224ca784f87723741d498932ddd
Eurmartins <victormartinssantos.work@gmail.com>
2025-11-02 00:46:41 -0300

    feat: update ServiceProvisionModule to include ServiceRequest entity and repository

--- src/infrastructure/modules/serviceprovision.module.ts
+++ src/infrastructure/modules/serviceprovision.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ServiceProvision } from 'src/domain/entities/serviceProvision.entity';
 import { Provider } from 'src/domain/entities/provider.entity';
+import { ServiceRequest } from 'src/domain/entities/serviceRequest.entity';
 import { ServiceProvisionService } from 'src/application/services/serviceProvision/serviceprovision.service';
 import { ServiceProvisionController } from '../controllers/serviceProvision.controller';
 import { AppLoggerService } from 'src/application/services/logger/logger.service';
@@ -9,9 +10,13 @@ import { SERVICE_PROVISION_REPOSITORY } from 'src/domain/repositories/servicePro
 import { ServiceProvisionRepositoryImpl } from '../repositories/serviceProvision.repository.impl';
 import { PROVIDER_REPOSITORY } from 'src/domain/repositories/provider.repository';
 import { ProviderRepositoryImpl } from '../repositories/provider.repository.impl';
+import { SERVICE_REQUEST_REPOSITORY } from 'src/domain/repositories/serviceRequest.repository';
+import { ServiceRequestRepositoryImpl } from '../repositories/serviceRequest.repository.impl';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ServiceProvision, Provider])],
+  imports: [
+    TypeOrmModule.forFeature([ServiceProvision, Provider, ServiceRequest]),
+  ],
   providers: [
     AppLoggerService,
     ServiceProvisionService,
@@ -22,6 +27,10 @@ import { ProviderRepositoryImpl } from '../repositories/provider.repository.impl
     {
       provide: PROVIDER_REPOSITORY,
       useClass: ProviderRepositoryImpl,
+    },
+    {
+      provide: SERVICE_REQUEST_REPOSITORY,
+      useClass: ServiceRequestRepositoryImpl,
     },
   ],
   controllers: [ServiceProvisionController],

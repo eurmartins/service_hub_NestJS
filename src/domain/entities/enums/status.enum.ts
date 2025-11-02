ddc4613553442b680ef2fc2182b54801f18bea41
Eurmartins <victormartinssantos.work@gmail.com>
2025-11-02 00:47:20 -0300

    feat: add ServiceRequestStatusEnum with various states for service requests

--- src/domain/entities/enums/status.enum.ts
+++ src/domain/entities/enums/status.enum.ts
@@ -2,3 +2,10 @@ export enum StatusEnum {
   ACTIVE = 'active',
   INACTIVE = 'inactive',
 }
+
+export enum ServiceRequestStatusEnum {
+  PENDING = 'pending',
+  IN_PROGRESS = 'in_progress',
+  COMPLETED = 'completed',
+  CANCELLED = 'cancelled',
+}

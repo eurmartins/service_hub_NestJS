ae1089134a4f00b1a36fe1429102bff02e31a77e
Eurmartins <victormartinssantos.work@gmail.com>
2025-11-17 23:57:15 -0300

    feat: add public access to getHello endpoint and integrate JwtAuthGuard in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientModule } from './infrastructure/modules/client.module';
@@ -10,6 +11,8 @@ import { ProviderModule } from './infrastructure/modules/provider.module';
 import { OrderServiceModule } from './infrastructure/modules/orderService.module';
 import { OrderModule } from './infrastructure/modules/order.module';
 import { RatingModule } from './infrastructure/modules/rating.module';
+import { AuthModule } from './infrastructure/modules/auth.module';
+import { JwtAuthGuard } from './infrastructure/guards/jwt-auth.guard';
 
 @Module({
   imports: [
@@ -22,8 +25,15 @@ import { RatingModule } from './infrastructure/modules/rating.module';
     OrderServiceModule,
     OrderModule,
     RatingModule,
+    AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
